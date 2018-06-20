6b67c5f985b8abb769821923411c5bad3651ff55
Ho Gia Bao <hogiabao2601@gmail.com>
2018-06-20 11:15:01 +0700

    init project

---
+++ src/main/java/com/bao/ho/JavaSpringBootExampleApplication.java
@@ -0,0 +1,12 @@
+package com.bao.ho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootExampleApplication.class, args);
+	}
+}
